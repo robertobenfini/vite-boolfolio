602f8e860c34a076108eb4084f076c1bb94c17fe
robertobenfini <roberto.benfini@gmail.com>
2023-08-29 16:21:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default {
+  components:{
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppMain />
+  </div>
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'AppMain',
+  data(){
+    return{
+      
+    }
+  },
+  created(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>ciao</h1>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style lang="">
+  
+</style>
