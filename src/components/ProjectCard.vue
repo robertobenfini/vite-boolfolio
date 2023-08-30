5d56e34bf1bf476ff1afab49139c2b49e6af8a29
robertobenfini <roberto.benfini@gmail.com>
2023-08-30 15:38:40 +0200

    [add] projectcard-bonus

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,59 @@
+<script>
+import { store } from '../store.js';
+
+
+export default {
+    name: "ProjectCard",
+    props:{
+        project: Object
+    },
+    data(){
+        return{
+            store,
+        }
+    },
+    methods: {
+        truncateText(text){
+          if(text.length > 100){
+            return text.substr(0, 50) + '...';
+          }
+    
+          return text
+        }
+}
+}
+</script>
+
+<template>
+
+    <div class="card my-3 min_height-377">
+        <div class="card-header">
+            {{ project.title }}
+        </div>
+        <div class="card-image-top">
+            <img :src="`${store.baseUrl}/storage/${project.image}`" alt="img">
+        </div>
+        <div class="card-body">
+            <div>
+            <span v-if="project.type">{{ project.type.name }}</span>
+            <span v-else>Categoria non assegnata</span>
+            </div>
+            <div v-if="project.technologies">
+            <span class="badge text-bg-primary me-3" v-for="technology in project.technologies" :key="technology.id">
+                {{ technology.name }}
+            </span>
+            </div>
+            <div>
+            {{ truncateText(project.content) }}
+            </div>
+        </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-sm btn-primary">Leggi il progetto</a>
+        </div>
+    </div>
+
+</template>
+
+<style lang="">
+    
+</style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 import { store } from '../store.js';
 import AppLoader from '../components/AppLoader.vue';
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
   name: 'ProjectList',
   components:{
-    AppLoader
+    AppLoader,
+    ProjectCard
   },
 
   data(){
@@ -44,13 +46,6 @@ export default {
       })
 
     },
-    truncateText(text){
-      if(text.length > 100){
-        return text.substr(0, 50) + '...';
-      }
-
-      return text
-    }
   }
 };
 </script>
@@ -71,31 +66,7 @@ export default {
     <div class="container">
       <div class="row">
         <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
-          <div class="card my-3 min_height-377">
-            <div class="card-header">
-              {{ project.title }}
-            </div>
-            <div class="card-image-top">
-              <img :src="`${store.baseUrl}/storage/${project.image}`" alt="img">
-            </div>
-            <div class="card-body">
-              <div>
-                <span v-if="project.type">{{ project.type.name }}</span>
-                <span v-else>Categoria non assegnata</span>
-              </div>
-              <div v-if="project.technologies">
-                <span class="badge text-bg-primary me-3" v-for="technology in project.technologies" :key="technology.id">
-                  {{ technology.name }}
-                </span>
-              </div>
-              <div>
-                {{ truncateText(project.content) }}
-              </div>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-sm btn-primary">Leggi il progetto</a>
-            </div>
-          </div>
+          <ProjectCard :project="project" />
         </div>
       </div>
     </div>
