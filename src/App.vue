28aff1a0ce326e0b242181a830ec32062c390c4e
robertobenfini <roberto.benfini@gmail.com>
2023-08-30 14:37:37 +0200

    [add] componente-header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
-import AppMain from './components/AppMain.vue'
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue'
 
 export default {
   components:{
-    AppMain
+    AppMain,
+    AppHeader
   }
 }
 </script>
 
 <template>
   <div>
+    <AppHeader/>
     <AppMain />
   </div>
 </template>
