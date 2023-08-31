6d1efb92cf138ad1cf2d7a7c2a7f31947e535e1e
robertobenfini <roberto.benfini@gmail.com>
2023-08-31 15:04:27 +0200

    [add] pagina-404

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/Homepage.vue';
 import ProjectList from './pages/ProjectList.vue';
 import SingleProject from './pages/SingleProject.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/archive/:slug',
             name: 'single-project',
             component: SingleProject
+        },
+        {
+            path: '/*',
+            name: 'not-found',
+            component: NotFound
         }
     ]
 });
