3cf7436083abe8783c920be58b0ca3d51bca07e1
robertobenfini <roberto.benfini@gmail.com>
2023-09-01 15:08:29 +0200

    [add] form-mail

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -53,8 +53,53 @@ export default {
 </script>
 
 <template>
-    <div>
-        
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="text-center">Contatti</h2>
+                </div>
+                <div v-if="success" class="col-12">
+                    <div class="alert alert-success">
+                        Messaggio inviato con successo
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <form @submit.prevent="sendForm()" class="row">
+                        <div class="col-12 col-md-6">
+                            <label class="control-label">Nome e Cognome</label>
+                            <input type="text" name="name" v-model="name" placeholder="Nome" class="form-control" :class="errors.name ? 'is-invalid' : ''">
+                            <p v-for="(error, index) in errors" :key="index" class="text-danger">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label class="control-label">Email</label>
+                            <input type="text" name="email" v-model="email" placeholder="Email" id="email" class="form-control" :class="errors.email ? 'is-invalid' : ''">
+                            <p v-for="(error, index) in errors" :key="index" class="text-danger">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label class="control-label">Contenuto</label>
+                            <textarea name="content" v-model="message" placeholder="Messaggio" id="content" class="form-control" :class="errors.content ? 'is-invalid' : ''">
+
+                            </textarea>
+                            <p v-for="(error, index) in errors" :key="index" class="text-danger">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-sm btn-success" type="submit" :disabled="loading">
+                                {{ loading ? 'Invio mail in corso' : 'Invia' }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
